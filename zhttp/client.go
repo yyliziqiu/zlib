a3d70a05cedf4ac514af42efcb9beec3d01e34a1
yyliziqiu <yyliziqiu@163.com>
2024-08-23 10:46:29 +0800

    promote zhttp

--- zhttp/client.go
+++ zhttp/client.go
@@ -24,15 +24,14 @@ const (
 )
 
 type Client struct {
-	client    *http.Client   //
-	logger    *logrus.Logger // 如果为 nil，则不记录日志
-	format    string         //
-	error     error          // 不能是指针
-	dumps     bool           // 将 HTTP 报文打印到控制台
-	baseURL   string         //
-	logLength int            // 日志最大长度
-	logEscape bool           // 替换日志中的特殊字符
-
+	client        *http.Client                   //
+	logger        *logrus.Logger                 // 如果为 nil，则不记录日志
+	format        string                         //
+	error         error                          // 不能是指针
+	dumps         bool                           // 将 HTTP 报文打印到控制台
+	baseURL       string                         //
+	logLength     int                            // 日志最大长度
+	logEscape     bool                           // 替换日志中的特殊字符
 	requestBefore func(req *http.Request)        // 在发送请求前调用
 	responseAfter func(res *http.Response) error // 在接收响应后调用
 }
@@ -303,7 +302,7 @@ func (cli *Client) PostForm(path string, query url.Values, header http.Header, i
 	return err
 }
 
-func (cli *Client) PostFile(path string, query url.Values, header http.Header, values map[string]string, files map[string]string, out interface{}) error {
+func (cli *Client) PostFormData(path string, query url.Values, header http.Header, values map[string]string, files map[string]string, out interface{}) error {
 	var (
 		buf    bytes.Buffer
 		writer = multipart.NewWriter(&buf)
@@ -395,4 +394,137 @@ func (cli *Client) writeFormFile(writer *multipart.Writer, key string, path stri
 	}
 
 	return nil
+}
+
+func (cli *Client) GetBinary(path string, query url.Values, header http.Header) ([]byte, string, error) {
+	req, err := cli.newRequest(http.MethodGet, path, query, header, nil)
+	if err != nil {
+		return nil, "", err
+	}
+
+	timer := zutil.NewTimer()
+
+	res, err := cli.doRequest(req)
+	if err != nil {
+		return nil, "", err
+	}
+	defer res.Body.Close()
+
+	bs, err := io.ReadAll(res.Body)
+
+	cli.logHTTP(HTTPLog{
+		Method:       http.MethodGet,
+		Request:      req,
+		RequestBody:  nil,
+		Response:     res,
+		ResponseBody: nil,
+		Error:        err,
+		Cost:         timer.Stops(),
+	})
+
+	return bs, res.Header.Get("Content-Type"), err
+}
+
+func (cli *Client) PostBinary(path string, query url.Values, header http.Header, mimeType string, in io.Reader, out interface{}) error {
+	req, err := cli.newRequest(http.MethodPost, path, query, header, in)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", mimeType)
+
+	timer := zutil.NewTimer()
+
+	res, err := cli.doRequest(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	resBody, err := cli.handleResponse(res, out)
+
+	cli.logHTTP(HTTPLog{
+		Method:       http.MethodPost,
+		Request:      req,
+		RequestBody:  nil,
+		Response:     res,
+		ResponseBody: resBody,
+		Error:        err,
+		Cost:         timer.Stops(),
+	})
+
+	return err
+}
+
+func (cli *Client) PostFile(path string, query url.Values, header http.Header, values map[string]string, filekey string, filepath string, out interface{}) error {
+	files := map[string]string{filekey: filepath}
+	return cli.PostFormData(path, query, header, values, files, out)
+}
+
+func (cli *Client) PostStream(path string, query url.Values, header http.Header, values map[string]string, filekey string, filename string, stream io.Reader, out interface{}) error {
+	var (
+		buf    bytes.Buffer
+		writer = multipart.NewWriter(&buf)
+	)
+
+	if len(values) > 0 {
+		for key, value := range values {
+			err := writer.WriteField(key, value)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if stream != nil {
+		part, err := writer.CreateFormFile(filekey, filename)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(part, stream)
+		if err != nil {
+			return err
+		}
+	}
+
+	err := writer.Close()
+	if err != nil {
+		return err
+	}
+
+	req, err := cli.newRequest(http.MethodPost, path, query, header, &buf)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	timer := zutil.NewTimer()
+
+	res, err := cli.doRequest(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	resBody, err := cli.handleResponse(res, out)
+
+	var bod []byte
+	if len(values) == 0 {
+		bod = []byte(fmt.Sprintf(`{"%s":"%s"}`, filekey, filename))
+	} else {
+		values[filekey] = filename
+		bod, _ = json.Marshal(values)
+	}
+	cli.logHTTP(HTTPLog{
+		Method:       http.MethodPost,
+		Request:      req,
+		RequestBody:  bod,
+		Response:     res,
+		ResponseBody: resBody,
+		Error:        err,
+		Cost:         timer.Stops(),
+	})
+
+	return err
 }
