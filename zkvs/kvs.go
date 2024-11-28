4c224b0df363691daf0598c702a18709c2b0c7fb
yyliziqiu <yyliziqiu@163.com>
2024-11-28 11:10:48 +0800

    优化 zkvs

--- zkvs/kvs.go
+++ zkvs/kvs.go
@@ -8,123 +8,98 @@ import (
 
 type KVS map[string]string
 
-func (k KVS) ShouldGet(key string) (string, bool) {
-	val, ok := k[key]
-	if !ok {
-		return "", false
+// 1
+
+func (k KVS) String(key string) (string, bool) {
+	if val, ok := k[key]; ok {
+		return strings.TrimSpace(val), true
 	}
-	return strings.TrimSpace(val), true
+	return "", false
 }
 
-func (k KVS) ShouldBool(key string) (bool, bool) {
-	val, ok := k.ShouldGet(key)
-	if !ok {
-		return false, false
+func (k KVS) Bool(key string) (bool, bool) {
+	if val, ok := k.String(key); ok {
+		return zconv.S2B(val), true
 	}
-	return zconv.S2B(val), true
+	return false, false
 }
 
-func (k KVS) ShouldInt(key string) (int, bool) {
-	val, ok := k.ShouldGet(key)
-	if !ok {
-		return 0, false
+func (k KVS) Int(key string) (int, bool) {
+	if val, ok := k.String(key); ok {
+		return zconv.S2I(val), true
 	}
-	return zconv.S2I(val), true
+	return 0, false
 }
 
-func (k KVS) ShouldInt64(key string) (int64, bool) {
-	val, ok := k.ShouldGet(key)
-	if !ok {
-		return 0, false
+func (k KVS) Int64(key string) (int64, bool) {
+	if val, ok := k.String(key); ok {
+		return zconv.S2I64(val), true
 	}
-	return zconv.S2I64(val), true
+	return 0, false
 }
 
-func (k KVS) ShouldFloat64(key string) (float64, bool) {
-	val, ok := k.ShouldGet(key)
-	if !ok {
-		return 0, false
+func (k KVS) Float64(key string) (float64, bool) {
+	if val, ok := k.String(key); ok {
+		return zconv.S2F64(val), true
 	}
-	return zconv.S2F64(val), true
+	return 0, false
 }
 
-func (k KVS) MustGet(key string, def string) string {
-	val, ok := k[key]
-	if !ok {
-		return def
+// 2
+
+func (k KVS) StringN(key string, def string) string {
+	if val, ok := k.String(key); ok {
+		return val
 	}
-	return strings.TrimSpace(val)
+	return def
 }
 
-func (k KVS) MustBool(key string, def bool) bool {
-	val, ok := k.ShouldGet(key)
-	if !ok {
-		return def
+func (k KVS) BoolN(key string, def bool) bool {
+	if val, ok := k.Bool(key); ok {
+		return val
 	}
-	return zconv.S2B(val)
+	return def
 }
 
-func (k KVS) MustInt(key string, def int) int {
-	val, ok := k.ShouldGet(key)
-	if !ok {
-		return def
+func (k KVS) IntN(key string, def int) int {
+	if val, ok := k.Int(key); ok {
+		return val
 	}
-	return zconv.S2I(val)
+	return def
 }
 
-func (k KVS) MustInt64(key string, def int64) int64 {
-	val, ok := k.ShouldGet(key)
-	if !ok {
-		return def
+func (k KVS) Int64N(key string, def int64) int64 {
+	if val, ok := k.Int64(key); ok {
+		return val
 	}
-	return zconv.S2I64(val)
+	return def
 }
 
-func (k KVS) MustFloat64(key string, def float64) float64 {
-	val, ok := k.ShouldGet(key)
-	if !ok {
-		return def
+func (k KVS) Float64N(key string, def float64) float64 {
+	if val, ok := k.Float64(key); ok {
+		return val
 	}
-	return zconv.S2F64(val)
+	return def
 }
+
+// 3
 
 func (k KVS) Get(key string) string {
-	val, _ := k.ShouldGet(key)
-	return val
-}
-
-func (k KVS) Bool(key string) bool {
-	val, _ := k.ShouldBool(key)
-	return val
-}
-
-func (k KVS) Int(key string) int {
-	val, _ := k.ShouldInt(key)
-	return val
-}
-
-func (k KVS) Int64(key string) int64 {
-	val, _ := k.ShouldInt64(key)
-	return val
-}
-
-func (k KVS) Float64(key string) float64 {
-	val, _ := k.ShouldFloat64(key)
-	return val
+	return k.StringN(key, "")
 }
 
 func (k KVS) Id() string {
-	return k.Get("id")
+	return k.StringN("id", "")
 }
 
 func (k KVS) Name() string {
-	return k.Get("name")
+	return k.StringN("name", "")
 }
 
 func (k KVS) Enabled() bool {
-	return k.Bool("enabled")
+	return k.BoolN("enabled", false)
 }
 
 func (k KVS) Disabled() bool {
-	return k.Bool("disabled")
+	return k.BoolN("disabled", false)
 }
